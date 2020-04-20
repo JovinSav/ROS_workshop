4f7e1177a8ae3bc29e6df7bc9a6514019bf52342
JovinS <39631282+JovinSav@users.noreply.github.com>
2020-04-20 15:21:19 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,17 +1,36 @@
 
-     var images = new Array()
+function changeImage()
+{
+    var img = document.getElementById("img");
+    img.src = images[x];
+    x++;
+
+    if(x >= images.length){
+        x = 0;
+    } 
+
+    fadeImg(img, 100, true);
+    setTimeout("changeImage()", 30000);
+}
+
+function fadeImg(el, val, fade){
+    if(fade === true){
+        val--;
+    }else{
+        val ++;
+    }
+
+    if(val > 0 && val < 100){
+        el.style.opacity = val / 100;
+        setTimeout(function(){fadeImg(el, val, fade);}, 10);
+    }
+}
+
+var images = [],
+x = 0;
+
      images[0] = "images/joint.png";
      images[1] = "images/link.png";
      images[2] = "images/link1.png";
-     setInterval("changeImage()", 30000);
-     var x=0;
 
-     function changeImage()
-     {
-                document.getElementById("img").src=images[x]
-                x++;
-                if (images.length == x) 
-                {
-                    x = 0;
-                }
-     }
+setTimeout("changeImage()", 30000);
